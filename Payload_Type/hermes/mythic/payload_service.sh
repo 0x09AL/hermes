0ef1fd294feccfeed11c8ea9d377109d74b71d4b
0x09AL <25826294+0x09AL@users.noreply.github.com>
2023-04-27 16:51:09 +0100

    Update payload_service.sh

--- Payload_Type/hermes/mythic/payload_service.sh
+++ Payload_Type/hermes/mythic/payload_service.sh
@@ -6,11 +6,13 @@ nohup python3 mythic_service.py &
 
 set +e
 
-if [ ! -d /sys/module/darling_mach ]; then
-	echo "ERROR: The darling-mach kernel module isn't loaded!"
-	exit 1
-fi
+#if [ ! -d /sys/module/darling_mach ]; then
+#	echo "ERROR: The darling-mach kernel module isn't loaded!"
+#	exit 1
+#fi
 
-export DYLD_ROOT_PATH=/usr/libexec/darling
+#export DYLD_ROOT_PATH=/usr/libexec/darling
 
-exec "${DYLD_ROOT_PATH}/usr/libexec/darling/vchroot" "${DYLD_ROOT_PATH}" /sbin/launchd
+#exec "${DYLD_ROOT_PATH}/usr/libexec/darling/vchroot" "${DYLD_ROOT_PATH}" /sbin/launchd
+
+darling shell
